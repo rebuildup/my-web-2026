0b29247b6a6572bfacbc325254aa72554bd8ab93
samuido <176191237+rebuildup@users.noreply.github.com>
2026-09-19 14:36:37 +0900

    chore: complete Cloudflare typegen sync

--- worker-configuration.d.ts
+++ worker-configuration.d.ts
@@ -16,6 +16,12 @@ declare namespace Cloudflare {
 	interface Env extends __BaseEnv_Env {}
 }
 interface Env extends __BaseEnv_Env {}
+type StringifyValues<EnvType extends Record<string, unknown>> = {
+	[Binding in keyof EnvType]: EnvType[Binding] extends string ? EnvType[Binding] : string;
+};
+declare namespace NodeJS {
+	interface ProcessEnv extends StringifyValues<Pick<Cloudflare.Env, "MY_WEB_2026_REACTIONS_TARGET">> {}
+}
 
 // Begin runtime types
 /*! *****************************************************************************
